0f459baf6e377752683c0a4a9a32dde874eb9c97
Dan King <king.daniel.thomas@gmail.com>
2024-01-30 12:31:14 +0700

    Un-comment-out the theme toggle button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -76,7 +76,7 @@ export const Header = forwardRef(function Header({ className }, ref) {
         <div className="hidden md:block md:h-5 md:w-px md:bg-zinc-900/10 md:dark:bg-white/15" />
         <div className="flex gap-4">
           <MobileSearch />
-          {/* <ThemeToggle />  Hide this since images don't invert */} 
+          <ThemeToggle />  
         </div>
         <div className="hidden min-[416px]:contents">
           <Button href="https://app.knokd.com/" target="_blank">Sign in</Button>
