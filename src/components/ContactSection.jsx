641dbc11574e6ade8873a761e293bf8d4c387283
Dan King <king.daniel.thomas@gmail.com>
2024-06-03 17:06:57 +0700

    fix: minor font-weight stuff

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -8,7 +8,7 @@ export function ContactSection () {
         <div className="white-top-heading">Knock on our Door</div>
         <p className="text-white text-4xl mt-[45px] mb-[23px]">Anything holding you back?</p>
         <div className="flex gap-6">
-        <Button variant="cta" target="_blank" href="mailto:support@knokd.com">Email Knokd Support</Button>
+        <Button variant="cta" target="_blank" href="mailto:support@knokd.com">Email Knokd support</Button>
         <img src="/images/get-started-arrow.svg" className="transform rotate-[320deg] scale-[90%] translate-x-[-10px] translate-y-[-10px] inline-block"></img>
         </div>
       </div>
