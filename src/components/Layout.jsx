a5e7faa6d5398fa82e6e05bffcb5b1e57071f0d9
Leszek W. Król <contact@leszekkrol.com>
2025-01-27 16:31:08 +0100

    Fixed lint issues

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,6 @@ import { motion } from 'framer-motion'
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-import { Logo } from '@/components/Logo'
 import { Navigation } from '@/components/Navigation'
 import { SectionProvider } from '@/components/SectionProvider'
 import { KnokdLogo } from './KnokdLogo'
